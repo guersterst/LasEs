cacd57f6b875090453a1f702c40a56ed8ff12286
Sebastian Vogt <vogt@fim.uni-passau.de>
2021-11-03 21:22:51 +0100

    continue digarmamsdmgf

--- docs/Entwurf/Klassendiagramm/src/persistence/repository/ScientificForumRepository.java
+++ docs/Entwurf/Klassendiagramm/src/persistence/repository/ScientificForumRepository.java
@@ -1,9 +1,18 @@
 package persistence.repository;
 
+import java.util.List;
+
+import dtos.ScientificForum;
 import persistence.util.ConnectionPool;
 
 public class ScientificForumRepository {
 	
 	private ConnectionPool connectionPool;
+	
+	public List<ScientificForum> search(String searchWord, int resultOffset, int resultLength) { return null; }
+	
+	public List<ScientificForum>
+	
+	
 
 }
