a60a193751d66672abe24b5a1dba9481620cf4ae
Sebastian Vogt <vogt@fim.uni-passau.de>
2021-11-05 00:28:52 +0100

    a hwi4ya7tzwr eghuf ga9weratg jguaheirghrueh ga84th saiurgheilrhsgufrgh zsieurhgzieurghaouhgadfhgksjdf gldjf gkshdfgaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- docs/Entwurf/Klassendiagramm/src/control/backing/NewSubmissionBacking.java
+++ docs/Entwurf/Klassendiagramm/src/control/backing/NewSubmissionBacking.java
@@ -1,12 +1,46 @@
 package control.backing;
 
+import java.util.List;
+
 import business.service.SubmissionService;
+import dtos.User;
 import idk.SessionInformation;
 
 public class NewSubmissionBacking {
 
+	
+	// Services and Session.
+	
 	private SessionInformation sessionInformation;
 	
 	private SubmissionService submissionService;
+	
+	// Inputs from view.
+	
+	private List<User> coAuthors;
+	
+	private String nameOfPaperInput;
+	
+	private String forumNameInput;
+	
+	private int editorSelectionInput;
+	
+	private byte[] pdfUpload;
+	
+	private String titleInput;
+	
+	private String coAuthorFirstNameInput;
+	
+	private String coAuthorLastNameInput;
+	
+	private String coAuthorEmailInput;
+	
+	// Action methods.
+	
+	public void uploadPDF() { }
+	
+	public void submitCoAuthor() { }
+	
+	public void submit() { }
 
 }
--- docs/Entwurf/Klassendiagramm/src/control/backing/VerificationBacking.java
+++ docs/Entwurf/Klassendiagramm/src/control/backing/VerificationBacking.java
@@ -1,10 +1,16 @@
 package control.backing;
 
+import java.time.LocalDateTime;
+
 import idk.SessionInformation;
 
 public class VerificationBacking {
 	
+	private LocalDateTime timerStart;
+	
 	private SessionInformation sessionInformation;
+	
+	public int secondsRemaining() { return 0; }
 
 	public String redirect() { return null; }
 
