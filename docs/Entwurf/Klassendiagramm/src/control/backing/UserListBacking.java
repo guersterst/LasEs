b6c0e1cae50518f32540282d6df399457eff84f4
guersterst <guerster@fim.uni-passau.de>
2021-11-05 01:40:53 +0100

    UserList

--- docs/Entwurf/Klassendiagramm/src/control/backing/UserListBacking.java
+++ docs/Entwurf/Klassendiagramm/src/control/backing/UserListBacking.java
@@ -9,4 +9,23 @@ public class UserListBacking {
 
 	private ProfileService profileService;
 
+	private Privilege privilege;
+
+	private String searchText;
+
+
+	public String applyFilter(){ }
+
+	public void nameUp(){ }
+
+	public void nameDown(){ }
+
+	public void roleUp(){ }
+
+	public void roleDown(){ }
+
+	public List<User> getUserList(){ return null;}
+
+
+
 }
