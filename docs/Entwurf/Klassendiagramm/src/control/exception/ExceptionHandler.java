1fce347fa93ed6990b6b6169850796c3e64d8123
Sebastian Vogt <vogt@fim.uni-passau.de>
2021-11-08 19:47:52 +0100

    add exception Handler

---
+++ docs/Entwurf/Klassendiagramm/src/control/exception/ExceptionHandler.java
@@ -0,0 +1,15 @@
+package control.exception;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeListenerProxy;
+
+public class ExceptionHandler extends PropertyChangeListenerProxy implements PropertyChangeListener {
+
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		// TODO Automatisch erstellter Methoden-Stub
+		
+	}
+
+}
