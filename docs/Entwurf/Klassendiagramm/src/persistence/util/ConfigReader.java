a92c4afa1195c760e4530e6eaf97f82e01435138
Sebastian Vogt <vogt@fim.uni-passau.de>
2021-11-09 13:33:24 +0100

    add config file reader

---
+++ docs/Entwurf/Klassendiagramm/src/persistence/util/ConfigReader.java
@@ -0,0 +1,13 @@
+package persistence.util;
+
+import dtos.GlobalConfig;
+
+public class ConfigReader {
+	
+	private static GlobalConfig config;
+	
+	public static void loadConfig() { }
+	
+	public static GlobalConfig getConfig() { return null; }
+
+}
