bfcfa74ff2b5fa053c54815980977abe1a837879
Sebastian Vogt <vogt@fim.uni-passau.de>
2021-11-09 16:35:27 +0100

    add verifications

---
+++ docs/Entwurf/Klassendiagramm/src/dtos/Verification.java
@@ -0,0 +1,17 @@
+package dtos;
+
+public class Verification {
+	
+	private int id;
+	
+	private String validationRandom;
+	
+	private boolean verified;
+	
+	private LocalDateTime timestampValidationStarted;
+	
+	private String nonValidatedEmailAddress;
+	
+	private int userId;
+
+}
