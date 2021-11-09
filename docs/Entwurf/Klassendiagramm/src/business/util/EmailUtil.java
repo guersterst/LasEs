0531a0c6400988a530a1e3c31989ef1c8769af6d
Sebastian Vogt <vogt@fim.uni-passau.de>
2021-11-09 14:20:59 +0100

    export svg

--- docs/Entwurf/Klassendiagramm/src/business/util/EmailUtil.java
+++ docs/Entwurf/Klassendiagramm/src/business/util/EmailUtil.java
@@ -7,5 +7,7 @@ public class EmailUtil {
 	public static void sendEmail(String sender, String recipient, String subject, String body) throws MessagingException { }
 	
 	public static String generateMailTo(String[] receiver, String[] cc, String subject, String body) { return null; }
+	
+	public static boolean checkEmailAddress(String address) { return false; }
 
 }
