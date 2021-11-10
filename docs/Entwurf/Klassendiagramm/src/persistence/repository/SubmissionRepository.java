71af4c89221566143d3bea76958b24c9cca5fd06
Sebastian Vogt <vogt@fim.uni-passau.de>
2021-11-10 10:11:42 +0100

    update submission repo

--- docs/Entwurf/Klassendiagramm/src/persistence/repository/SubmissionRepository.java
+++ docs/Entwurf/Klassendiagramm/src/persistence/repository/SubmissionRepository.java
@@ -9,14 +9,21 @@ import persistence.exception.InvalidQueryParamsException;
 
 public class SubmissionRepository {
 	
-	public static List<Submission> getListWithSearchword(String searchword, ResultListParameters resultParams, Transaction transaction) throws InvalidQueryParamsException { return null; }
+	// get, add, change by id
+	public static Submission getSubmission(Submission submission, Transaction transaction) throws InvalidFieldsException { return null; }
+	public static void addSubmission(Submission submission, Transaction transaction) throws InvalidFieldsException { }
+	public static void changeSubmission(Submission old, Submission edited, Transaction transaction) throws InvalidFieldsException { }
 	
+	
+	// get lists
+	public static List<Submission> getListWithSearchword(String searchword, ResultListParameters resultParams, Transaction transaction) throws InvalidQueryParamsException { return null; }
 	public static List<Submission> getList(ResultListParameters resultParams, Transaction transaction) throws InvalidQueryParamsException { return null; }
 	
-	public static Submission getSubmission(int id, Transaction transaction) throws IllegalArgumentException { return null; }
-	
-	public static void addSubmission(Submission submission, Transaction transaction) throws InvalidFieldsException { }
-	
-	public static void changeSubmission(int submissionId, Submission edited, Transaction transaction) { }
+	// get lists for
+	public static List<Submission> getListForAuthor() { return null; }
+	public static List<Submission> getListForCoAuthor() { return null; }
+	public static List<Submission> getListForEditor() { return null; }
+	public static List<Submission> getListForReviewer() { return null; }
+	public static List<Submission> getListForScientificForum() { return null; }
 
 }
