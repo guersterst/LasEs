d00f25d0060ae36b26829d9a431bbe34c51074b5
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2021-11-16 14:20:02 +0100

    constraints for user

--- docs/Feinspezi/ext_files/db/createUser.sql
+++ docs/Feinspezi/ext_files/db/createUser.sql
@@ -1,15 +1,19 @@
 CREATE TABLE "user" (
 	id SERIAL PRIMARY KEY NOT NULL,
 	email_address VARCHAR UNIQUE NOT NULL,
-	firstname VARCHAR,
-	lastname VARCHAR,
+	firstname VARCHAR NOT NULL,
+	lastname VARCHAR NOT NULL,
 	title VARCHAR,
 	employer VARCHAR,
 	birthdate DATE,
 	avatar_thumbnail BYTEA,
 	is_registered BOOLEAN NOT NULL,
 	password_hash VARCHAR,
-	password_salt VARCHAR
+	password_salt VARCHAR,
+	
+	CONSTRAINT valid_name CHECK (length(firstname) >= 1 AND length(lastname) >= 1),
+	CONSTRAINT valid_birthdate CHECK (birthdate < CURRENT_DATE),
+	CONSTRAINT email_address_pattern CHECK (email_address LIKE '_%@_%._%')
 );
 
 CREATE TABLE reviewer(
@@ -26,3 +30,5 @@ CREATE TABLE administrator(
 	id INTEGER PRIMARY KEY NOT NULL,
 	FOREIGN KEY (id) REFERENCES "user"(id) ON DELETE CASCADE
 );
+
+CREATE UNIQUE INDEX email_unique_case_insensitive on "user" (LOWER(email_address));
