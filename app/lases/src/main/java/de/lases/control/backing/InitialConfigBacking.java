693ac5d15591f613852b2a6c750c6ca13f489e32
Sebastian Vogt <vogt32@ads.uni-passau.de>
2021-11-16 14:02:57 +0100

    document InitalConfigbacking and add dto for database connection state

--- app/lases/src/main/java/de/lases/control/backing/InitialConfigBacking.java
+++ app/lases/src/main/java/de/lases/control/backing/InitialConfigBacking.java
@@ -3,21 +3,44 @@ package de.lases.control.backing;
 import de.lases.business.service.CustomizationService;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
+/**
+ * Backing bean for the initial configuration page.
+ */
 @RequestScoped
 @Named
 public class InitialConfigBacking {
 
+    @Inject
     private CustomizationService customizationService;
 
-    private String dbConnectionState;
+    private String datasourceConnectionState;
 
+    /**
+     * Load the state of the database connection into this bean.
+     */
     @PostConstruct
     public void init() {
     }
 
-    public void createDb() {
+    /**
+     * Create the datasource and return the welcome page.
+     *
+     * @return Show the welcome page.
+     */
+    public String createDatasource() {
+        return null;
     }
 
+    /**
+     * Get information about the connection state of the datasource and an
+     * error description if it is not connected.
+     *
+     * @return Information about the connection state of the datasource.
+     */
+    public String getDatasourceConnectionState() {
+        return datasourceConnectionState;
+    }
 }
