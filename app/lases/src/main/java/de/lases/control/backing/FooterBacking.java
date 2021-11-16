cc1875f57ab667049425785836f580a8000e702f
Sebastian Vogt <vogt32@ads.uni-passau.de>
2021-11-16 20:45:59 +0100

    add FooterBacking

---
+++ app/lases/src/main/java/de/lases/control/backing/FooterBacking.java
@@ -0,0 +1,26 @@
+package de.lases.control.backing;
+
+import de.lases.control.internal.SessionInformation;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+/**
+ * Backing bean for the footer
+ */
+@Named
+@RequestScoped
+public class FooterBacking {
+
+    @Inject
+    private SessionInformation sessionInformation;
+
+    public SessionInformation getSessionInformation() {
+        return sessionInformation;
+    }
+
+    public void setSessionInformation(SessionInformation sessionInformation) {
+        this.sessionInformation = sessionInformation;
+    }
+
+}
