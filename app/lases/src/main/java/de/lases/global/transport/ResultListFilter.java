7f85b7cd4b9aa6f9dbe088307edd6e47f695ef44
Sebastian Vogt <vogt32@ads.uni-passau.de>
2021-11-17 16:09:33 +0100

    document ResultListFilter

--- app/lases/src/main/java/de/lases/global/transport/ResultListFilter.java
+++ app/lases/src/main/java/de/lases/global/transport/ResultListFilter.java
@@ -11,4 +11,27 @@ public class ResultListFilter {
 
     private List<ResultListFilterOption> filterOptions;
 
+    public String getColumnName() {
+        return columnName;
+    }
+
+    /**
+     * Set the name of the table column this filter should apply to.
+     *
+     * @param columnName Name of the column for this filter.
+     */
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    /**
+     * Get the list of filter options that should be applied to the column
+     * specified via {@code setColumnName}.
+     *
+     * @return List of filter options.
+     */
+    public List<ResultListFilterOption> getFilterOptions() {
+        return filterOptions;
+    }
+
 }
