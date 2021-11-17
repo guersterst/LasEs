bc70503184c491ef5fee47d363495afaa7b8bbfa
Sebastian Vogt <vogt32@ads.uni-passau.de>
2021-11-17 21:16:30 +0100

    document SubmissionState

--- app/lases/src/main/java/de/lases/global/transport/SubmissionState.java
+++ app/lases/src/main/java/de/lases/global/transport/SubmissionState.java
@@ -4,5 +4,24 @@ package de.lases.global.transport;
  * Represents a submission state.
  */
 public enum SubmissionState {
-    SUBMITTED, REVISION_REQUIRED, REJECTED, ACCEPTED;
+
+    /**
+     * The submission was submitted and its state was modified no further.
+     */
+    SUBMITTED,
+
+    /**
+     * The submission can still be accepted, but a revision is required.
+     */
+    REVISION_REQUIRED,
+
+    /**
+     * The submission was rejected and will not be further considered.
+     */
+    REJECTED,
+
+    /**
+     * The submission was accepted as is.
+     */
+    ACCEPTED;
 }
