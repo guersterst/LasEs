602dede77f38b893bb00d43edada377ab9466e3e
johannes-garstenauer <64467583+johannes-garstenauer@users.noreply.github.com>
2021-11-17 16:14:34 +0100

    RegistrationService JDOC

--- app/lases/src/main/java/de/lases/business/service/RegistrationService.java
+++ app/lases/src/main/java/de/lases/business/service/RegistrationService.java
@@ -9,19 +9,33 @@ import jakarta.inject.Named;
 import de.lases.persistence.repository.*;
 import de.lases.global.transport.*;
 
-@ApplicationScoped
-@Named
+/**
+ * Provides functionality for the registration and creation of users.
+ */
+@Dependent
 public class RegistrationService {
 
     private Event<UIMessage> uiMessageEvent;
 
     private Transaction transaction;
 
-    public User selfRegister(User user) {
+    /**
+     * Registers and creates a regular user in the database.
+     *
+     * @param user The users data.
+     */
+    public void selfRegister(User user) {
         return null;
     }
 
+    /**
+     * Registers and creates a user in the database.
+     * This user may be an admin.
+     *
+     * @param user The users data.
+     */
     public void registerByAdmin(User user) {
+
     }
 
 }
