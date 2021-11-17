0692a7e80e09ab7808bfbe540cf46db9aa79feb3
Sebastian Vogt <vogt32@ads.uni-passau.de>
2021-11-17 16:50:01 +0100

    document ResultListFilterOption

--- app/lases/src/main/java/de/lases/global/transport/ResultListFilterOption.java
+++ app/lases/src/main/java/de/lases/global/transport/ResultListFilterOption.java
@@ -9,4 +9,30 @@ public class ResultListFilterOption {
 
     private String comparand;
 
+    public ResultListFilterComparator getComparator() {
+        return comparator;
+    }
+
+    /**
+     * Set the comparator, which is an operation that compares two values. For
+     * reference, see {@link ResultListFilterComparator}
+     *
+     * @param comparator
+     */
+    public void setComparator(ResultListFilterComparator comparator) {
+        this.comparator = comparator;
+    }
+
+    public String getComparand() {
+        return comparand;
+    }
+
+    /**
+     * Set the value that should be on the right side of the comparison.
+     *
+     * @param comparand The comparand.
+     */
+    public void setComparand(String comparand) {
+        this.comparand = comparand;
+    }
 }
