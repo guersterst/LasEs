51ced3b2701d576f11aa54f731e92f45eced36c2
Sebastian Vogt <vogt32@ads.uni-passau.de>
2021-11-17 17:34:38 +0100

    document Style.java

--- app/lases/src/main/java/de/lases/global/transport/Style.java
+++ app/lases/src/main/java/de/lases/global/transport/Style.java
@@ -4,5 +4,24 @@ package de.lases.global.transport;
  * Represents a user interface style.
  */
 public enum Style {
-    ORANGE, DARK, BLUE, GREEN
+
+    /**
+     * An orange style.
+     */
+    ORANGE,
+
+    /**
+     * A dark style.
+     */
+    DARK,
+
+    /**
+     * A blue style.
+     */
+    BLUE,
+
+    /**
+     * A green style.
+     */
+    GREEN
 }
