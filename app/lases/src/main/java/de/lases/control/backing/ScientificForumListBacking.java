edcdda54d2384bb00d86f28642347e16cd82c36e
Sebastian Vogt <vogt32@ads.uni-passau.de>
2021-11-17 09:54:00 +0100

    document ScientificForumListBacking

--- app/lases/src/main/java/de/lases/control/backing/ScientificForumListBacking.java
+++ app/lases/src/main/java/de/lases/control/backing/ScientificForumListBacking.java
@@ -5,30 +5,75 @@ import de.lases.control.internal.*;
 import de.lases.global.transport.*;
 import jakarta.annotation.PostConstruct;
 import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
+import java.io.Serial;
 import java.io.Serializable;
 
+/**
+ * Backing bean for the scientific forum list page.
+ */
 @ViewScoped
 @Named
 public class ScientificForumListBacking implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = 288940091897301232L;
+
+    @Inject
     private SessionInformation sessionInformation;
+
+    @Inject
+    private ScientificForumService scientificForumService;
+
+    private DateSelect deadlineFilterSelect;
 
     private Pagination<ScientificForum> scientificForumPagination;
 
-    private ScientificForumService scientificService;
-
-    private DateSelect deadline;
-
-    private String searchString;
-
+    /**
+     * Initialize the list by getting the first page from the data source.
+     */
     @PostConstruct
     public void init() {
     }
 
+    /**
+     * Apply the selected deadline filter.
+     */
     public void applyFilters() {
     }
 
+    public DateSelect getDeadlineFilterSelect() {
+        return deadlineFilterSelect;
+    }
+
+    /**
+     * Set the selected filter option for filtering forums after deadline
+     *
+     * @param dateFilterSelectSub The selected filter option for filtering
+     *                            forums after deadline.
+     */
+    public void setDeadlineFilterSelect(DateSelect deadlineFilterSelect) {
+        this.deadlineFilterSelect = deadlineFilterSelect;
+    }
+
+    /**
+     * Get the pagination for the search results with scientific forums.
+     *
+     * @return The pagination for scientific forums.
+     */
+    public Pagination<ScientificForum> getScientificForumPagination() {
+        return scientificForumPagination;
+    }
+
+    /**
+     * Get session information.
+     *
+     * @return The session information.
+     */
+    public SessionInformation getSessionInformation() {
+        return sessionInformation;
+    }
 
 }
