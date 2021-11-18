ef00aeeeadbbf8087f7f653d1397df08930abcc5
Sebastian Vogt <vogt32@ads.uni-passau.de>
2021-11-18 16:36:37 +0100

    document scienceFieldRepository.java

--- app/lases/src/main/java/de/lases/persistence/repository/ScienceFieldRepository.java
+++ app/lases/src/main/java/de/lases/persistence/repository/ScienceFieldRepository.java
@@ -3,33 +3,152 @@ package de.lases.persistence.repository;
 import de.lases.global.transport.*;
 import de.lases.persistence.exception.*;
 
+import javax.xml.crypto.Data;
 import java.util.List;
 
+/**
+ * Offers get/add/remove operations on a science field and the
+ * possibility to get lists of science fields.
+ */
 public class ScienceFieldRepository {
 
-    // get, add, change, remove by id
-    public static ScienceField get(Submission submission, Transaction transaction) throws InvalidFieldsException {
+    /**
+     * Takes a science field dto that is filled out with a valid id or a valid
+     * name and returns a fully filled science field dto.
+     *
+     * @param scienceField A ScienceField dto that must be filled with a valid
+     *                     id.
+     * @param transaction The transaction to use.
+     * @return A fully filled ScienceField dto.
+     * @throws NotFoundException If there is no science field with the provided
+     *                           id or name.
+     * @throws InvalidFieldsException If both name and id are provided, but they
+     *                                belong to two different science fields.
+     * @throws DatasourceQueryFailedException If the datasource cannot be
+     *                                        queried.
+     */
+    public static ScienceField get(ScienceField scienceField,
+                                   Transaction transaction)
+            throws NotFoundException{
         return null;
     }
 
-    public static void add(Submission submission, Transaction transaction) throws InvalidFieldsException {
+    /**
+     * Adds a science field to the repository.
+     *
+     * @param scienceField A fully filled science field dto.
+     * @param transaction The transaction to use.
+     * @throws DataNotWrittenException If writing the data to the repository
+     *                                 fails.
+     * @throws KeyExistsException If there is already a science field with the
+     *                            same id.
+     * @throws InvalidFieldsException If one of the fields of the science field
+     *                                is null.
+     * @throws DatasourceQueryFailedException If the datasource cannot be
+     *                                        queried.
+     */
+    public static void add(ScienceField scienceField, Transaction transaction)
+            throws DataNotWrittenException, KeyExistsException {
     }
 
-    //Todo why change?
-    public static void change(Submission old, Submission edited, Transaction transaction) throws InvalidFieldsException {
+    /**
+     * Takes a science field dto that is filled with a valid id and removes
+     * this science field from the repository.
+     *
+     * @param scienceField The science field to remove. Must be filled with a
+     *                     valid id.
+     * @param transaction The transaction to use.
+     * @throws NotFoundException The specified science field was not found in
+     *                           the repository.
+     * @throws DataNotWrittenException If writing the data to the repository
+     * @throws DatasourceQueryFailedException If the datasource cannot be
+     *                                        queried.
+     */
+    public static void remove(ScienceField scienceField, Transaction transaction)
+            throws NotFoundException, DataNotWrittenException {
     }
 
-    public static void remove(Submission submission, Transaction transaction) throws InvalidFieldsException {
-    }
-
-
-    public static List<ScienceField> getList(Transaction transaction) {
+    /**
+     * Gets a list all science fields that belong to the specified scientific
+     * forum.
+     *
+     * @param forum A {@code ScientificForum} dto with a valid id.
+     * @param transaction The transaction to use.
+     * @param resultListParameters The ResultListParameters dto that results
+     *                             parameters from the pagination like
+     *                             filtering, sorting or number of elements.
+     * @return A list of fully filled science field dtos for all reviews that
+     *         belong to the specified submission.
+     * @throws DataNotCompleteException If the list is truncated.
+     * @throws NotFoundException If there is no scientific forum with the provided
+     *                           id.
+     * @throws DatasourceQueryFailedException If the datasource cannot be
+     *                                        queried.
+     * @throws InvalidQueryParamsException If the resultListParameters contain
+     *                                     an erroneous option.
+     */
+    public static List<ScienceField> getList(ScientificForum forum,
+                                             Transaction transaction,
+                                             ResultListParameters
+                                                     resultListParameters)
+            throws DataNotCompleteException, NotFoundException {
         return null;
     }
 
+    /**
+     * Gets a list all science fields that belong to the specified user.
+     *
+     * @param user A {@code User} dto with a valid id.
+     * @param transaction The transaction to use.
+     * @param resultListParameters The ResultListParameters dto that results
+     *                             parameters from the pagination like
+     *                             filtering, sorting or number of elements.
+     * @return A list of fully filled science field dtos for all reviews that
+     *         belong to the specified user.
+     * @throws DataNotCompleteException If the list is truncated.
+     * @throws NotFoundException If there is no user with the provided id.
+     * @throws DatasourceQueryFailedException If the datasource cannot be
+     *                                        queried.
+     * @throws InvalidQueryParamsException If the resultListParameters contain
+     *                                     an erroneous option.
+     */
+    public static List<ScienceField> getList(User user, Transaction transaction,
+                                             ResultListParameters
+                                                     resultListParameters)
+            throws DataNotCompleteException, NotFoundException {
+        return null;
+    }
+
+    /**
+     * Gets a list all science fields.
+     *
+     * @param transaction The transaction to use.
+     * @param resultListParameters The ResultListParameters dto that results
+     *                             parameters from the pagination like
+     *                             filtering, sorting or number of elements.
+     * @return A list of fully filled science field dtos.
+     * @throws DataNotCompleteException If the list is truncated.
+     * @throws DatasourceQueryFailedException If the datasource cannot be
+     *                                        queried.
+     * @throws InvalidQueryParamsException If the resultListParameters contain
+     *                                     an erroneous option.
+     */
+    public static List<ScienceField> getList(Transaction transaction,
+                                             ResultListParameters
+                                                     resultListParameters)
+            throws DataNotCompleteException {
+        return null;
+    }
+
+    /**
+     * Checks if a science field with the given name exists in the repository.
+     *
+     * @param name The name to check.
+     * @param transaction The transaction to use.
+     * @return Does the given name exist as science field in the repository.
+     */
     public static boolean isScienceField(String name, Transaction transaction) {
         return false;
     }
 
-    //todo maybe imlplement equals and hash for issciencefield
 }
