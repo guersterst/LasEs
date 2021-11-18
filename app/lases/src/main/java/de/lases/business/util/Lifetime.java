91b2c1e59c67ff94b203e49e9233640837623f86
johannes-garstenauer <64467583+johannes-garstenauer@users.noreply.github.com>
2021-11-18 14:03:16 +0100

    LifeTime finish

--- app/lases/src/main/java/de/lases/business/util/Lifetime.java
+++ app/lases/src/main/java/de/lases/business/util/Lifetime.java
@@ -1,9 +1,14 @@
 package de.lases.business.util;
 
+/**
+ * Handles the system's start and shutdown.
+ */
 public class Lifetime {
 
+    /**
+     * Initiates the system's required startup operations and adds a shutdown hook.
+     */
     public static void startup() {
 
     }
-
 }
