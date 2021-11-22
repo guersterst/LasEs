2bfc10f1029aed71fffe7749603d85580249e7c6
Sebastian Vogt <vogt32@ads.uni-passau.de>
2021-11-22 16:08:13 +0100

    document trespass listener

--- app/lases/src/main/java/de/lases/control/internal/TrespassListener.java
+++ app/lases/src/main/java/de/lases/control/internal/TrespassListener.java
@@ -7,19 +7,35 @@ import jakarta.faces.event.PhaseListener;
 
 public class TrespassListener implements PhaseListener {
 
-    // @Inject
-    //private final SessionInformation sessionInformation
-    //        = CDI.current().select(SessionInformation.class).get();
+    private final SessionInformation sessionInformation
+            = CDI.current().select(SessionInformation.class).get();
 
+    /**
+     * Gets the is of the phase this listener is interested in.
+     *
+     * @return The id of the restore view phase.
+     */
     @Override
     public PhaseId getPhaseId() {
         return null;
     }
 
+    /**
+     * Before the restore view phase, this listener does nothing.
+     *
+     * @param event The event that is about to happen.
+     */
     @Override
     public void beforePhase(PhaseEvent event) {
     }
 
+    /**
+     * After the restore view phase, we check if the user has the rights to
+     * visit the site he has requested. If not, he will be redirected to a 404
+     * error page.
+     *
+     * @param event The event that just happened.
+     */
     @Override
     public void afterPhase(PhaseEvent event) {
     }
