61d6a55275776dde02204a2773d0a6e5fd6da567
guersterst <guerster@fim.uni-passau.de>
2021-11-22 15:23:08 +0100

    We are able to use constants now

--- app/lases/src/main/java/de/lases/control/internal/Constants.java
+++ app/lases/src/main/java/de/lases/control/internal/Constants.java
@@ -1,4 +1,4 @@
-package de.lases.control;
+package de.lases.control.internal;
 
 /**
  * Holds all constants for validating maximal length.
@@ -8,70 +8,70 @@ public final class Constants {
     /**
      * Maximal Length that is allowed for the first name.
      */
-    private static final String FIRST_NAME_MAX = "50";
+    public static final String FIRST_NAME_MAX = "50";
 
     /**
      * Maximal Length that is allowed for the last name.
      */
-    private static final String LAST_NAME_MAX = "50";
+    public static final String LAST_NAME_MAX = "50";
 
     /**
      * Maximal Length that is allowed for the title.
      */
-    private static final  String TITLE_MAX = "20";
+    public static final  String TITLE_MAX = "20";
 
     /**
      * Maximal Length that is allowed for the employee's name.
      */
-    private static final String EMPLOYER_MAX ="100";
+    public static final String EMPLOYER_MAX ="100";
 
     /**
      * Maximal Length that is allowed for the email.
      */
-    private static final String EMAIL_MAX = "70";
+    public static final String EMAIL_MAX = "70";
 
     /**
      * Maximal Length that is allowed for the science field.
      */
-    private static final String SCIENCE_FIELD_MAX = "30";
+    public static final String SCIENCE_FIELD_MAX = "30";
 
     /**
      * Maximal Length that is allowed for the name of a forum.
      */
-    private static final String FORUM_NAME_MAX = "200";
+    public static final String FORUM_NAME_MAX = "200";
 
     /**
      * Maximal Length that is allowed for the description of a forum.
      */
-    private static final String FORUM_DESCRIPTION_MAX = "500";
+    public static final String FORUM_DESCRIPTION_MAX = "500";
 
     /**
      * Maximal Length that is allowed for the manual of a review.
      */
-    private static final String REVIEW_MANUAL_MAX = "500";
+    public static final String REVIEW_MANUAL_MAX = "500";
 
     /**
      * Maximal Length that is allowed for the url of a forum.
      */
-    private static final String FORUM_URL_MAX = "150";
+    public static final String FORUM_URL_MAX = "150";
 
     /**
      * Maximal Length that is allowed for the title of a submission.
      */
-    private static final String SUBMISSION_TITLE = "200";
+    public static final String SUBMISSION_TITLE = "200";
 
     /**
      * Maximal Length that is allowed for the name of a company.
      */
-    private static final String SYSTEM_COMPANY_NAME = "100";
+    public static final String SYSTEM_COMPANY_NAME = "100";
 
     /**
      * Maximal Length that is allowed for the heading of the welcome page.
      */
-    private static final String WELCOME_HEADING_MAX = "200";
+    public static final String WELCOME_HEADING_MAX = "200";
 
     /**
      * Maximal Length that is allowed for the description of the system.
      */
-    private static final String WELCOME_DESCRIPTION_MAX = "500";
+    public static final String WELCOME_DESCRIPTION_MAX = "500";
 }
