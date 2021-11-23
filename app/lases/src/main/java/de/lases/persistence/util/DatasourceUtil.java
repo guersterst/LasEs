e59d3c4a60aa17ba1800a986fd3a897bc2228b2c
Sebastian Vogt <vogt32@ads.uni-passau.de>
2021-11-23 09:58:45 +0100

    document DatasourceUtil

---
+++ app/lases/src/main/java/de/lases/persistence/util/DatasourceUtil.java
@@ -0,0 +1,27 @@
+package de.lases.persistence.util;
+
+import de.lases.global.transport.ConnectionState;
+
+/**
+ * Offers methods for managing the datasource.
+ */
+public class DatasourceUtil {
+
+    /**
+     * Deletes all users/email addresses (if the user already has a valid email
+     * address he will not be deleted) from the datasource that need to verify
+     * their email address but did not do so within 3 days.
+     */
+    public static void cleanUpVerifications() {
+    }
+
+    /**
+     * Tests if the datasource is connected.
+     *
+     * @return the connection state of the datasource.
+     */
+    public static ConnectionState testDatasourceConnection() {
+        return null;
+    }
+
+}
