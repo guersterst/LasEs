c039f42fe8b77db6c4a1c5650e22b9d901693cac
johannes-garstenauer <64467583+johannes-garstenauer@users.noreply.github.com>
2021-11-25 22:24:40 +0100

    Util and internal

--- app/lases/src/main/java/de/lases/business/util/Hashing.java
+++ app/lases/src/main/java/de/lases/business/util/Hashing.java
@@ -9,11 +9,11 @@ public class Hashing {
     }
 
     /**
-     * Generates a random salt for increased entropy.
+     * Generates a random salt for increased entropy in hashing.
      * The salts are 128 Bit long and are returned as base64 strings with
      * 24 characters.
      *
-     * @return Returns the random salt.
+     * @return The random salt.
      */
     public static String generateRandomSalt() {
         return null;
@@ -37,7 +37,7 @@ public class Hashing {
      *
      * @param password The password to be hashed.
      * @param salt The salt for increased entropy.
-     * @return Returns the hashed and salted password.
+     * @return The hashed and salted password.
      */
     public static String hashWithGivenSalt(String password, String salt) {
         return null;
