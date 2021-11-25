9e74d46f8cd59ea8af49bfc1be72412db9949229
johannes-garstenauer <64467583+johannes-garstenauer@users.noreply.github.com>
2021-11-25 16:17:25 +0100

    Constructor doc

--- app/lases/src/main/java/de/lases/control/internal/UncheckedExceptionHandlerFactory.java
+++ app/lases/src/main/java/de/lases/control/internal/UncheckedExceptionHandlerFactory.java
@@ -8,6 +8,15 @@ import jakarta.faces.context.ExceptionHandlerFactory;
 public class UncheckedExceptionHandlerFactory extends ExceptionHandlerFactory {
     private final ExceptionHandlerFactory parent;
 
+    /**
+     * Constructs an {@link UncheckedExceptionHandlerFactory}.
+     * <p>
+     * Do not call this method, it is used internally
+     * as a factory and registered in the {@code faces-config.xml}
+     * as such.
+     * </p>
+     * @param parent The parent {@link ExceptionHandlerFactory}.
+     */
     @SuppressWarnings("deprecation")
     public UncheckedExceptionHandlerFactory(ExceptionHandlerFactory parent) {
         this.parent = parent;
