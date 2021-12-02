00c58dbf498517713c4f0a25fc93a39803ccfa10
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2021-12-02 21:33:45 +0100

    remove overly long line

--- docs/Implementierungsplan/tests/LoginServiceTest.java
+++ docs/Implementierungsplan/tests/LoginServiceTest.java
@@ -3,7 +3,7 @@ class LoginServiceTest {
 
     private static final String SECURE_EXAMPLE_PASSWORD = "Affe123!";
     private static final String VERY_RANDOM_SALT_BASE64 = "QWZmZW4gc2luZCBzdXBlcg==";
-    // PBKDF2WithHmacSHA256, 128 bit, 75000 iterations. https://gist.github.com/schicho/616fb9529b624000a577344388ce3f3b
+    // PBKDF2WithHmacSHA256, 128 bit, 75000 iterations.
     private static final String SECURE_PASSWORD_HASH_BASE64 = "YXirI3Xsv1hhne/qI+um6Q==";
 
     @Test
