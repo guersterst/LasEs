41089bea19735d3608ec69047828e747ed16d084
Sebastian Vogt <vogt32@ads.uni-passau.de>
2021-12-02 21:15:24 +0100

    add ConnectionPoolTest

--- docs/Implementierungsplan/tests/ConnectionPoolTest.java
+++ docs/Implementierungsplan/tests/ConnectionPoolTest.java
@@ -1,18 +1,3 @@
-package de.lases.persistence.repository;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.postgresql.jdbc.PgConnection;
-import org.postgresql.util.HostSpec;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Properties;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 @ExtendWith(MockitoExtension.class)
 class ConnectionPoolTest {
 
@@ -35,7 +20,7 @@ class ConnectionPoolTest {
         ConnectionPool.init();
         ConnectionPool pool = ConnectionPool.getInstance();
         assertThrows(IllegalArgumentException.class, () -> {
-                pool.releaseConnection(Mockito.mock(Connection.class));
+            pool.releaseConnection(Mockito.mock(Connection.class));
         });
     }
 
