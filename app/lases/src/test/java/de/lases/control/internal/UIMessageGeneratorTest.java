6a54401cb5e2dcd01cb0a20a9000409ff40eafa1
johannes-garstenauer <64467583+johannes-garstenauer@users.noreply.github.com>
2021-12-02 16:05:51 +0100

    git vomit

---
+++ app/lases/src/test/java/de/lases/control/internal/UIMessageGeneratorTest.java
@@ -0,0 +1,59 @@
+package de.lases.control.internal;
+
+import de.lases.business.service.SubmissionService;
+import de.lases.global.transport.MessageCategory;
+import de.lases.global.transport.Submission;
+import de.lases.global.transport.UIMessage;
+import jakarta.enterprise.event.Event;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class UIMessageGeneratorTest {
+
+    @Mock
+    SubmissionService mockedService;
+
+    @Inject
+    Event<UIMessage> uiMessageEvent;
+
+    @Inject
+    FacesContext facesContext;
+
+    static UIMessage uiMessage;
+
+    @BeforeAll
+    static void setEventBehaviour() {
+        //todo is this correct here, mock resbundle?
+        uiMessage = new UIMessage("Internal Error", MessageCategory.ERROR);
+    }
+
+    @Test
+    void eventFiredTest() {
+        Mockito.when(mockedService.get(Mockito.any(Submission.class))).thenAnswer(
+                invocationOnMock -> {
+                    uiMessageEvent.fire(uiMessage);
+                    return null;
+                }
+        );
+
+        SubmissionService submissionService = new SubmissionService();
+        Submission submission = new Submission();
+        submission.setId(1);
+        submissionService.get(submission);
+
+        List<FacesMessage> messageList = facesContext.getMessageList();
+        assertEquals(messageList.get(0).getDetail(), uiMessage.getMessage());
+    }
+}
