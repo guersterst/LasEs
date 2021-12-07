2de8971d66efa68f8965389decd70f49c4d5abf7
guersterst <guerster@fim.uni-passau.de>
2021-12-07 14:59:39 +0100

    testgetPaper

--- app/lases/src/test/java/de/lases/persistence/repository/PaperRepositoryTest.java
+++ app/lases/src/test/java/de/lases/persistence/repository/PaperRepositoryTest.java
@@ -3,6 +3,7 @@ package de.lases.persistence.repository;
 import de.lases.global.transport.FileDTO;
 import de.lases.global.transport.Paper;
 import de.lases.persistence.exception.DataNotWrittenException;
+import de.lases.persistence.exception.NotFoundException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -27,7 +28,7 @@ class PaperRepositoryTest {
         paper = new Paper();
         paper.setSubmissionId(5);
         paper.setUploadTime(LocalDateTime.now());
-        paper.setVersionNumber(2);
+        paper.setVersionNumber(3);
         paper.setVisible(false);
         pdf = new FileDTO();
         pdf.setFile(new byte[]{1, 2, 3, 4});
@@ -55,21 +56,52 @@ class PaperRepositoryTest {
 //    }
 
     @Test
+    void testGetPaper() throws SQLException, NotFoundException, DataNotWrittenException {
+        PaperRepository.add(paper,pdf, transaction);
+
+        Connection connection = transaction.getConnection();
+        PreparedStatement statement = connection.prepareStatement(
+                """
+                        SELECT * FROM paper
+                        WHERE  submission_id = ? AND version = ?
+                        """
+        );
+        statement.setInt(1, paper.getSubmissionId());
+        statement.setInt(2, paper.getVersionNumber());
+
+        ResultSet resultSet = statement.executeQuery();
+
+        Paper resultPaper = new Paper();
+        if (resultSet.next()) {
+            resultPaper.setSubmissionId(resultSet.getInt("submission_id"));
+            resultPaper.setVisible(resultSet.getBoolean("is_visible"));
+            resultPaper.setVersionNumber(resultSet.getInt("version"));
+            resultPaper.setUploadTime(resultSet.getTimestamp("timestamp_upload").toLocalDateTime());
+        }
+
+        assertEquals(resultPaper, PaperRepository.get(resultPaper, transaction));
+    }
+
+    @Test
     void testAdd() throws SQLException, DataNotWrittenException {
         Connection conn = transaction.getConnection();
         PreparedStatement stmt = conn.prepareStatement(
                 """
-                SELECT * FROM paper
-                """);
+                        SELECT * FROM paper
+                        """);
         ResultSet resultSet = stmt.executeQuery();
         int i = 0;
-        while (resultSet.next()) { i++; }
+        while (resultSet.next()) {
+            i++;
+        }
 
         PaperRepository.add(paper, pdf, transaction);
 
         ResultSet resultSet2 = stmt.executeQuery();
         int j = 0;
-        while (resultSet2.next()) { j++; }
+        while (resultSet2.next()) {
+            j++;
+        }
 
         assertEquals(1, j - i);
     }
