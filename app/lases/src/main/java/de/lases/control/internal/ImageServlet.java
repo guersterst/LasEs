4a01a9cd2f0acda758dd314e8e7306dda0cc6073
johannes-garstenauer <garstenaue@fim.uni-passau.de>
2021-12-08 18:54:43 +0100

    Finish url param-parsing and access rights to get image

--- app/lases/src/main/java/de/lases/control/internal/ImageServlet.java
+++ app/lases/src/main/java/de/lases/control/internal/ImageServlet.java
@@ -1,9 +1,17 @@
 package de.lases.control.internal;
 
 import java.io.IOException;
+import java.lang.reflect.MalformedParametersException;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 import de.lases.business.service.CustomizationService;
 import de.lases.business.service.UserService;
+import de.lases.global.transport.FileDTO;
+import de.lases.global.transport.Privilege;
+import de.lases.global.transport.User;
+import de.lases.persistence.exception.InvalidFieldsException;
 import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,10 +19,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.swing.text.html.Option;
+
 /**
  * Serves images under the /image/* url.
  */
-@WebServlet("/image/*")
+@WebServlet("/image*")
 public class ImageServlet extends HttpServlet {
 
     @Inject
@@ -23,21 +33,62 @@ public class ImageServlet extends HttpServlet {
     @Inject
     private CustomizationService customizationService;
 
+    @Inject
+    private SessionInformation sessionInformation;
+
+    private static final Logger logger = Logger.getLogger(ImageServlet.class.getName());
+
     /**
      * Answers a get request that requests a specific image resource specified
      * over the url. Redirects to a 404 page if the user does not have the
      * rights to see the resource.
-     * The logo is served under /image/logo, the avatars under
-     * /image/avatar/userId
+     * The logo is served under {@code /image?type=logo}, the avatars under
+     * {@code /image?type=avatar&user=userId}.
+     * Here the URL parameter {@code type} determines whether the logo or an avatar
+     * is requested. The other URL parameter {@code user} expects a valid userId to determine
+     * which user's avatar to deliver.
      *
-     * @param request The servlet request.
+     * @param request  The servlet request.
      * @param response The servlet response.
      * @throws ServletException If the request for the GET could not be handled.
-     * @throws IOException If an input or output error is detected when the
-     *                     servlet handles the GET request.
+     * @throws IOException      If an input or output error is detected when the
+     *                          servlet handles the GET request.
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (Objects.equals(request.getParameter("type"), "logo")) {
+
+            deliverRequestedImage(response, true, Optional.empty());
+        } else if (Objects.equals(request.getParameter("type"), "avatar")) {
+            User user = sessionInformation.getUser();
+            Integer urlUserId = Integer.getInteger(request.getParameter("user"));
+
+            if (user == null || user.getId() == null || user.getPrivileges() == null) {
+
+                logger.severe("There must be a user in the session in order to request a user's avatar."
+                        + "This user must contain an id and privileges.");
+                throw new InvalidFieldsException();
+            } else if (user.isAdmin() || user.getPrivileges().contains(Privilege.EDITOR)) {
+
+                deliverRequestedImage(response, false, Optional.of(urlUserId));
+            } else if (user.isRegistered() && Objects.equals(user.getId(), urlUserId)) {
+
+                deliverRequestedImage(response, false, Optional.of(urlUserId));
+            } else {
+                logger.severe("An avatar was requested where the required privileges were not found."
+                        + " Requested was user: " + urlUserId + "avatar.");
+                throw new IllegalCallerException();
+            }
+        } else {
+            logger.warning("A request for an image could not be handled due to the type URL"
+                    + "parameter being invalid: type=" + request.getParameter("type"));
+            throw new MalformedParametersException();
+        }
+    }
+
+    private void deliverRequestedImage(HttpServletResponse response, boolean isLogo, Optional<Integer> userId) {
+
     }
 
 }
