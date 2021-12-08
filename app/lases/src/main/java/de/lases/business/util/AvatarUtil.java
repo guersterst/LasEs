97aafc27be1ad8a4bc112c68cca8aa50239c174f
johannes-garstenauer <garstenaue@fim.uni-passau.de>
2021-12-08 17:21:36 +0100

    Add practical test.

--- app/lases/src/main/java/de/lases/business/util/AvatarUtil.java
+++ app/lases/src/main/java/de/lases/business/util/AvatarUtil.java
@@ -6,11 +6,7 @@ import de.lases.persistence.exception.InvalidFieldsException;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.*;
-import java.nio.Buffer;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.logging.Logger;
 
 /**
@@ -19,21 +15,21 @@ import java.util.logging.Logger;
  */
 public final class AvatarUtil {
 
-    private static final int IMAGE_WIDTH = 100;
+    private static final int IMAGE_WIDTH = 150;
 
-    private static final int IMAGE_HEIGHT = 100;
+    private static final int IMAGE_HEIGHT = 150;
 
     private static final Logger logger = Logger.getLogger(AvatarUtil.class.getName());
 
     /**
      * Generates a small thumbnail from a given image.
      *
-     * @param file The {@link FileDTO} containing an image as a byte array.
+     * @param imageFile The {@link FileDTO} containing an image as a byte array.
      * @return The image converted into a thumbnail.
      * @throws IOException If the creation of an image and its scaling has failed.
      */
-    public static FileDTO generateThumbnail(FileDTO file) throws IOException {
-        if (file.getFile() == null) {
+    public static FileDTO generateThumbnail(FileDTO imageFile) throws IOException {
+        if (imageFile.getFile() == null) {
             throw new InvalidFieldsException();
         }
 
@@ -42,10 +38,11 @@ public final class AvatarUtil {
         try {
 
             // Create image instance, scale and write to an output stream.
-
-            Image scaledImg = getImageFromByteArray(file).getScaledInstance(IMAGE_WIDTH, IMAGE_HEIGHT,
+            Image scaledImg = getBufferedImageFromByteArray(imageFile).getScaledInstance(IMAGE_WIDTH, IMAGE_HEIGHT,
                     BufferedImage.SCALE_SMOOTH);
             ImageIO.write(castToBufferedImage(scaledImg), "jpg", bos);
+            bos.flush();
+            bos.close();
         } catch (IOException e) {
             logger.severe("An error in writing or reading an image has occurred.");
             throw new IOException();
@@ -62,7 +59,7 @@ public final class AvatarUtil {
      * @return The byte array as an {@code Image}.
      * @throws IOException If the conversion fails.
      */
-    private static BufferedImage getImageFromByteArray(FileDTO file) throws IOException {
+    private static BufferedImage getBufferedImageFromByteArray(FileDTO file) throws IOException {
         InputStream byteStream = new ByteArrayInputStream(file.getFile());
         return ImageIO.read(byteStream);
     }
@@ -74,7 +71,7 @@ public final class AvatarUtil {
 
         // Create a buffered image with transparency.
         BufferedImage bImage = new BufferedImage(img.getWidth(null), img.getHeight(null),
-                BufferedImage.TYPE_INT_ARGB);
+                BufferedImage.TYPE_INT_RGB);
 
         // Draw the image onto the buffered image.
         Graphics2D bGr = bImage.createGraphics();
@@ -85,15 +82,22 @@ public final class AvatarUtil {
     }
 
     public static void main(String[] args) throws IOException {
-        FileDTO fileDTO = new FileDTO();
-        InputStream is = AvatarUtil.class.getClassLoader().getResourceAsStream("face.jpg");
-        fileDTO.setFile(is.readAllBytes());
 
-        FileDTO thumbnail = AvatarUtil.generateThumbnail(fileDTO);
+        // Get image.
+        BufferedImage img = ImageIO.read(AvatarUtil.class.getClassLoader().getResource("face.jpg"));
+
+        // Create fileDTO.
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(img, "jpg", baos);
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setFile(baos.toByteArray());
+
+        // Write the image.
+        FileDTO thumbnail = generateThumbnail(fileDTO);
+        BufferedImage bufferedThumbnail = getBufferedImageFromByteArray(thumbnail);
         File outputFile = new File("thumbnail.jpg");
-        FileOutputStream out = new FileOutputStream(outputFile);
-        out.write(thumbnail.getFile());
-        out.close();
+        ImageIO.write(bufferedThumbnail, "jpg", outputFile);
+
     }
 
 }
