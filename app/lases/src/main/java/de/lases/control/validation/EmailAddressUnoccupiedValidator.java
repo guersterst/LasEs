2e548239176fcf81373d9263eb73e7bdbf2d2de7
Thomas Kirz <thomas.kirz@gmail.com>
2021-12-14 18:33:45 +0100

    Fix service injection other validator

--- app/lases/src/main/java/de/lases/control/validation/EmailAddressUnoccupiedValidator.java
+++ app/lases/src/main/java/de/lases/control/validation/EmailAddressUnoccupiedValidator.java
@@ -9,7 +9,6 @@ import jakarta.faces.context.FacesContext;
 import jakarta.faces.validator.FacesValidator;
 import jakarta.faces.validator.Validator;
 import jakarta.faces.validator.ValidatorException;
-import jakarta.inject.Inject;
 
 import java.util.PropertyResourceBundle;
 import java.util.logging.Logger;
@@ -21,9 +20,6 @@ import java.util.logging.Logger;
  */
 @FacesValidator
 public class EmailAddressUnoccupiedValidator implements Validator<String> {
-
-    @Inject
-    private UserService userService;
 
     private final Logger l = Logger.getLogger(EmailAddressUnoccupiedValidator.class.getName());
 
@@ -40,6 +36,7 @@ public class EmailAddressUnoccupiedValidator implements Validator<String> {
                          String address) throws ValidatorException {
         User user = new User();
         user.setEmailAddress(address);
+        UserService userService = CDI.current().select(UserService.class).get();
         if (userService.emailExists(user)) {
             PropertyResourceBundle bundle = CDI.current().select(PropertyResourceBundle.class).get();
             l.finer("Validation failed: " + address + " is already in use.");
