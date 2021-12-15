bd22ac3b6c7a41678aa1c31845f83c884541d737
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2021-12-15 17:50:28 +0100

    sql fixxxx

--- app/lases/CREATE_ALL.sql
+++ app/lases/CREATE_ALL.sql
@@ -130,7 +130,7 @@ CREATE TABLE verification (
 	timestamp_validation_started TIMESTAMP,
 	unvalidated_email_address VARCHAR,
   	PRIMARY KEY (id),
-	FOREIGN KEY (id) REFERENCES "user"(id)
+	FOREIGN KEY (id) REFERENCES "user"(id) ON DELETE CASCADE
 );
 
 CREATE TABLE system(
