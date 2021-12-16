d90e50c8434821c10999a40c0acd9ea639ab7772
Stefanie Guerster <guerster@nautilus.fim.uni-passau.de>
2021-12-16 23:15:57 +0100

    finished administration

--- app/lases/src/main/java/de/lases/control/backing/AdministrationBacking.java
+++ app/lases/src/main/java/de/lases/control/backing/AdministrationBacking.java
@@ -1,5 +1,6 @@
 package de.lases.control.backing;
 
+import de.lases.business.internal.ConfigPropagator;
 import de.lases.business.service.CustomizationService;
 import de.lases.control.exception.IllegalAccessException;
 import de.lases.global.transport.*;
@@ -10,11 +11,22 @@ import jakarta.faces.context.FacesContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.servlet.http.Part;
+import org.jboss.weld.util.reflection.Reflections;
+import org.primefaces.shaded.commons.io.IOUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PropertyResourceBundle;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * Backing bean for the administration page.
@@ -33,6 +45,9 @@ public class AdministrationBacking {
 
     @Inject
     private transient PropertyResourceBundle resourceBundle;
+
+     @Inject
+     private ConfigPropagator configPropagator;
 
     private static final Logger logger = Logger.getLogger(AdministrationBacking.class.getName());
 
@@ -137,8 +152,61 @@ public class AdministrationBacking {
         return PATH_TO_STYLE_DIRECTORY.concat(systemSettings.getStyle() + ".css");
     }
 
-    public String[] getStyles() {
-        InputStream inputStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/resources/" +  PATH_TO_STYLE_DIRECTORY);
-        return null;
+    public String[] getStyles() throws IOException {
+        String[] stylsheets = configPropagator.getProperty("STYLE").split("#");
+
+        /*
+        List<File> files = new ArrayList<>();
+
+        try {
+            files = getAllStyleSheets();
+
+            for (int i = 0; i < files.size(); i++) {
+                String name = filenames.get(i).substring(0, filenames.get(i).lastIndexOf(".css"));
+                filenames.add(name);
+            }
+
+        } catch (URISyntaxException | IOException e) {
+            logger.log(Level.WARNING, e.getMessage());
+        }
+         */
+
+
+
+        /*
+
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("/resources/" +  PATH_TO_STYLE_DIRECTORY);
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))){
+
+            String resource;
+
+            while ((resource = bufferedReader.readLine()) != null) {
+                filenames.add(resource);
+            }
+
+        } catch (IOException exception) {
+            logger.severe("While reading the directory of stylesheets an exception occurred.");
+        }
+
+         */
+
+        /*
+        if (!filenames.isEmpty()) {
+            List<String> result = new ArrayList<>();
+
+            for (int i = 0; i < filenames.size(); i++) {
+                String name = filenames.get(i).replace(".css", "");
+                result.add(name);
+            }
+
+            return result;
+        }
+         */
+
+        /*
+        IOUtils.readLines(getClass().getClassLoader().getResourceAsStream("resources/" + PATH_TO_STYLE_DIRECTORY));
+         */
+
+        return stylsheets;
     }
 }
