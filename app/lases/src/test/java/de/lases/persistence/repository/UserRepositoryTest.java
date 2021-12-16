ad71641c803c86efff26e4b8fd7cc8aebb501eba
Thomas Kirz <thomas.kirz@gmail.com>
2021-12-16 23:21:03 +0100

    Make UserRepositoryTest work

--- app/lases/src/test/java/de/lases/persistence/repository/UserRepositoryTest.java
+++ app/lases/src/test/java/de/lases/persistence/repository/UserRepositoryTest.java
@@ -1,6 +1,5 @@
 package de.lases.persistence.repository;
 
-import de.lases.business.service.UserService;
 import de.lases.global.transport.FileDTO;
 import de.lases.global.transport.User;
 import de.lases.persistence.exception.NotFoundException;
@@ -22,6 +21,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(WeldJunit5Extension.class)
 class UserRepositoryTest {
 
+    private static final String EXAMPLE_EMAIL_ADDRESS = "john.doe@foo.bar";
+    private static final String EXAMPLE_FIRST_NAME = "John";
+    private static final String EXAMPLE_LAST_NAME = "Doe";
+    private static final String EXAMPLE_HASH = "thisissuppposedtobeahash";
+    private static final String EXAMPLE_SALT = "Potassium boride";
+
     @WeldSetup
     public WeldInitiator weld = WeldInitiator.from(ConnectionPool.class, ConfigReader.class, ConfigReader.class)
             .activate(RequestScoped.class, SessionScoped.class).build();
@@ -30,6 +35,7 @@ class UserRepositoryTest {
      * Unfortunately we have to do this before every single test, since @BeforeAll methods are static and static
      * methods don't work with our weld plugin.
      */
+
     @BeforeEach
     void startConnectionPool() {
         FileDTO file = new FileDTO();
@@ -48,15 +54,18 @@ class UserRepositoryTest {
         ConnectionPool.shutDown();
     }
 
-    private static final String EXAMPLE_EMAIL_ADDRESS = "john.doe@foo.bar";
-
     @Test
     void testAddEmailExists() throws Exception {
+        Transaction transaction = new Transaction();
+
         User user = new User();
         user.setEmailAddress(EXAMPLE_EMAIL_ADDRESS);
-
-        Transaction transaction = new Transaction();
+        user.setFirstName(EXAMPLE_FIRST_NAME);
+        user.setLastName(EXAMPLE_LAST_NAME);
+        user.setPasswordHashed(EXAMPLE_HASH);
+        user.setPasswordSalt(EXAMPLE_SALT);
         UserRepository.add(user, transaction);
+
         assertTrue(UserRepository.emailExists(user, transaction));
         transaction.abort();
     }
