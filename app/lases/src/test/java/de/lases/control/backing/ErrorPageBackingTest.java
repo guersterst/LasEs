50215b22c79fb07095e2e19eda650475400d1675
Thomas Kirz <thomas.kirz@gmail.com>
2022-01-06 15:13:26 +0100

    Make ErrorPageBackingTest work

--- app/lases/src/test/java/de/lases/control/backing/ErrorPageBackingTest.java
+++ app/lases/src/test/java/de/lases/control/backing/ErrorPageBackingTest.java
@@ -2,19 +2,33 @@ package de.lases.control.backing;
 
 import de.lases.business.internal.ConfigPropagator;
 import de.lases.global.transport.ErrorMessage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class ErrorPageBackingTest {
 
     @Mock ConfigPropagator configPropagator;
 
+    private final ErrorPageBacking errorPageBacking = new ErrorPageBacking();
+
+    @BeforeEach
+    void setBackingDependencies() throws Exception {
+        Field configPropagatorField = errorPageBacking.getClass().getDeclaredField("configPropagator");
+        configPropagatorField.setAccessible(true);
+        configPropagatorField.set(errorPageBacking, configPropagator);
+    }
+
     @Test
     void testErrorPageBacking() {
-        ErrorPageBacking errorPageBacking = new ErrorPageBacking();
         ErrorMessage errorMessage = new ErrorMessage("test", "test");
         errorPageBacking.setErrorMessage(errorMessage);
         assertEquals(errorMessage, errorPageBacking.getErrorMessage());
@@ -22,15 +36,13 @@ class ErrorPageBackingTest {
 
     @Test
     void testDevelopmentModeOn() {
-        ErrorPageBacking errorPageBacking = new ErrorPageBacking();
         when(configPropagator.getProperty("DEBUG_AND_TEST_MODE")).thenReturn("true");
         assertTrue(errorPageBacking.isDevelopmentMode());
     }
 
     @Test
     void testDevelopmentModeOff() {
-        ErrorPageBacking errorPageBacking = new ErrorPageBacking();
-        when(configPropagator.getProperty("DEBUG")).thenReturn("false");
+        when(configPropagator.getProperty("DEBUG_AND_TEST_MODE")).thenReturn("false");
         assertFalse(errorPageBacking.isDevelopmentMode());
 
     }
