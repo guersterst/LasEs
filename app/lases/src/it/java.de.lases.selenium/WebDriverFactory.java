46302bf4bddd210fa53708f68b029551108c31ca
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2022-01-17 16:16:30 +0100

    reindent

--- app/lases/src/it/java.de.lases.selenium/WebDriverFactory.java
+++ app/lases/src/it/java.de.lases.selenium/WebDriverFactory.java
@@ -1,16 +1,15 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxDriverService;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class WebDriverFactory {
 
-  public static String LOCALHOST_URL = "http://localhost:8080/lases_war_exploded/";
+    public static String LOCALHOST_URL = "http://localhost:8080/lases_war_exploded/";
 
-  public static WebDriver createFirefoxWebDriver() {
-    FirefoxOptions firefoxOptions = new FirefoxOptions();
-    firefoxOptions.addArguments("--allow-insecure-localhost");
+    public static WebDriver createFirefoxWebDriver() {
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--allow-insecure-localhost");
 
-    return new FirefoxDriver(firefoxOptions);
-  }
+        return new FirefoxDriver(firefoxOptions);
+    }
 }
