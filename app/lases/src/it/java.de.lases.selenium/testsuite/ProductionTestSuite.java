35a022e336aa08b42f94013cff104f3d84a01815
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2022-01-17 17:37:48 +0100

    finalize example work

--- app/lases/src/it/java.de.lases.selenium/testsuite/ProductionTestSuite.java
+++ app/lases/src/it/java.de.lases.selenium/testsuite/ProductionTestSuite.java
@@ -1,7 +1,9 @@
+package testsuite;
+
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({FirstTestClass.class, SecondTestClass.class})
-public class IntegrationTestSuite {
+@SelectClasses({})
+public class ProductionTestSuite {
 }
