336331178d09febb03d68019df7ac0cecd0128f3
Sebastian Vogt <vogt@fim.uni-passau.de>
2022-01-18 19:21:13 +0100

    first test works

--- app/lases/src/it/java/de/lases/selenium/stress/UserRegistersClicksRandomlyAndDeletesHimself.java
+++ app/lases/src/it/java/de/lases/selenium/stress/UserRegistersClicksRandomlyAndDeletesHimself.java
@@ -2,57 +2,38 @@ package de.lases.selenium.stress;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.Keys;
 import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class UserRegistersClicksRandomlyAndDeletesHimself {
     private WebDriver driver;
     private Map<String, Object> vars;
     JavascriptExecutor js;
+
     @BeforeEach
     public void setUp() {
         driver = new FirefoxDriver();
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
     }
+
     @AfterEach
     public void tearDown() {
         driver.quit();
     }
-    public String waitForWindow(int timeout) {
-        try {
-            Thread.sleep(timeout);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Set<String> whNow = driver.getWindowHandles();
-        Set<String> whThen = (Set<String>) vars.get("window_handles");
-        if (whNow.size() > whThen.size()) {
-            whNow.removeAll(whThen);
-        }
-        return whNow.iterator().next();
-    }
+
     @Test
     public void userRegistersClicksRandomlyAndDeletesHimself() {
         driver.get("http://localhost:8082/lases_1_0_SNAPSHOT_war/");
         driver.manage().window().maximize();
+        js.executeScript("window.scroll(0, document.documentElement.offsetHeight);");
 
-        driver.findElement(By.id("j_idt33:register-cbtn")).click();
+        driver.findElement(By.id("register-frm:register-cbtn")).click();
         driver.findElement(By.id("register-frm:first-name-itxt")).click();
         driver.findElement(By.id("register-frm:first-name-itxt")).sendKeys("Stressor");
         driver.findElement(By.id("register-frm:last-name-itxt")).click();
@@ -69,7 +50,6 @@ public class UserRegistersClicksRandomlyAndDeletesHimself {
         driver.findElement(By.id("nav-profile-link")).click();
         driver.findElement(By.id("global-search-frm:search-btn")).click();
         driver.findElement(By.id("nav-profile-link")).click();
-        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scroll(0, document.documentElement.offsetHeight);");
         driver.findElement(By.id("edit-profile-form:employer-itxt")).click();
         driver.findElement(By.id("edit-profile-form:employer-itxt")).sendKeys("Stressiger Job");
