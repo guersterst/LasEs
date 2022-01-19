86b6b5368bda2e1a7cddc10c29687a62b0364c09
Sebastian Vogt <vogt@fim.uni-passau.de>
2022-01-19 23:53:19 +0100

    'fix' bugs where exceptions came

--- app/lases/src/it/java/de/lases/selenium/stress/Stress.java
+++ app/lases/src/it/java/de/lases/selenium/stress/Stress.java
@@ -4,39 +4,49 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.*;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 public class Stress {
 
-    public static final int USER_CREATION = 30;
+    public static final int USER_CREATION = 3;
 
-    public static final int FORUM_SUBMISSION = 20;
+    public static final int FORUM_SUBMISSION = 2;
 
     public static final int THREADS = FORUM_SUBMISSION + USER_CREATION;
 
     public static final String URL = "http://ds9.fim.uni-passau.de:8002/lases/";
 
-    static ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(THREADS);
-
     public static void main(String... args) throws ExecutionException, InterruptedException, FileNotFoundException {
 
-        Deque<Future<List<ResponseTimeEntry>>> futures = new ArrayDeque<>();
+        Deque<List<ResponseTimeEntry>> futures = new ConcurrentLinkedDeque<>();
+
+        List<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < USER_CREATION; i++) {
-            futures.push(executor.submit(new UserRegistersClicksRandomlyAndDeletesHimself()));
+            Thread thread = new Thread(() -> {
+                UserRegistersClicksRandomlyAndDeletesHimself u = new UserRegistersClicksRandomlyAndDeletesHimself();
+                futures.push(u.call());
+            });
+            threads.add(thread);
+            thread.start();
         }
         for (int i = 0; i < FORUM_SUBMISSION; i++) {
-            futures.push(executor.submit(new UserCreatesForumAndSubmission()));
+            Thread thread = new Thread(() -> {
+                UserCreatesForumAndSubmission u = new UserCreatesForumAndSubmission();
+                futures.push(u.call());
+            });
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread: threads) {
+            thread.join();
         }
 
         Map<String, List<Long>> responseTimes = new HashMap<>();
 
-        for (int i = 0; i < THREADS; i++) {
-            executor.
-            List<ResponseTimeEntry> responseTimeEntries = futures.pop().get();
+        while (!futures.isEmpty()) {
+            List<ResponseTimeEntry> responseTimeEntries = futures.pop();
             for (ResponseTimeEntry responseTimeEntry: responseTimeEntries) {
                 String actionName = responseTimeEntry.actionName();
                 Long responseTime = responseTimeEntry.responseTime();
@@ -45,7 +55,6 @@ public class Stress {
             }
         }
         System.out.println(responseTimes);
-        executor.shutdown();
 
         File csvOutput = new File("out.csv");
         try (PrintWriter pw = new PrintWriter(csvOutput)) {
