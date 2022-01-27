4e474dbfcf9fe05d9f4da5eee5d185313e4b8ffc
Sebastian Vogt <vogt@fim.uni-passau.de>
2022-01-27 17:27:22 +0100

    delete aaaaaa example

--- app/lases/src/it/java/de/lases/selenium/example/ExampleTestSuite.java
+++
@@ -1,9 +0,0 @@
-package de.lases.selenium.example;
-
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectClasses({FirstTestClass.class, SecondTestClass.class, AAAAATEst.class})
-public class ExampleTestSuite {
-}
